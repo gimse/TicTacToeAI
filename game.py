b4d8164f9e36cd2902e8053ad0ecfa606d3e3198
skitrening <23360355+skitrening@users.noreply.github.com>
2018-04-18 16:39:06 +0200

    Setting random player to start

--- game.py
+++ game.py
@@ -29,7 +29,6 @@ def inputPlayerLetter():
     else:
         return ['O', 'X']
 def whoGoesFirst():
-    return 'computer'
     # Randomly choose the player who goes first.
     if random.randint(0, 1) == 0:
         return 'computer'
